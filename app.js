8115eec2fabfa6afabd6b088ced2784c138ce976
Anushka Raj <anush6633@gmail.com>
2020-09-11 22:13:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+
+let input=document.querySelector('.city');
+let weatherData = document.querySelector('.weather-data');
+let btn=document.querySelector('.submit');
+btn.addEventListener('click', ()=>{
+    displayWeather();
+    
+})
+
+document.addEventListener('keypress',(event)=>{
+    if(event.keyCode===13){
+        displayWeather();
+    }
+})
+
+function displayWeather(){
+    let place = input.value;
+    console.log(place);
+
+    let api="https://api.openweathermap.org/data/2.5/find?q="+ place +"&units=metric&appid=60dc55b1b64f048387de957a5efe4ab2";
+    console.log(api);
+    fetch(api)
+    .then(response=>{
+        data=response.json();
+        return data;
+})
+    .then(data=>{
+        console.log(data);
+     //access data from the fetched object 
+        let temp=data.list[0].main.feels_like;
+        let weather=data.list[0].weather[0].description;
+        let windSpeed=data.list[0].wind.speed;
+        let maxTemp=data.list[0].main.temp_max;
+        let minTemp=data.list[0].main.temp_min;
+        let humidity=data.list[0].main.humidity;
+        let pressure=data.list[0].main.pressure;
+
+     //display weather
+        document.querySelector('.temp').textContent=temp + "\u00B0 C";
+        document.querySelector('.weather').textContent=weather;       
+        document.querySelector('.wind-speed').textContent=windSpeed + " kph";
+        document.querySelector('.min-temp').textContent=minTemp ;
+        document.querySelector('.max-temp').textContent=maxTemp;
+        document.querySelector('.humidity').textContent=humidity + " %";
+        document.querySelector('.pressure').textContent=pressure + " Pa.";
+        document.querySelector('.place').textContent=place;
+        
+     //change icon for different weathers
+        if( weather=='moderate rain')
+        weatherIcon.setAttribute('src','images/moderate.png');
+        else if(weather=='haze')
+        weatherIcon.setAttribute('src','images/haze.png');
+        else if(weather=='scattered clouds')
+        weatherIcon.setAttribute('src','images/scattered.png');
+        else if(weather=='light rain')
+        weatherIcon.setAttribute('src','images/light.png');
+        else if(weather=='overcast clouds')
+        weatherIcon.setAttribute('src','images/overcast.png');
+
+    })
+    .catch(error=>{
+        console.log(error);
+        alert('Please enter a valid location.');
+})
+
+
+//daily forecast
+let forecastapi="https://api.openweathermap.org/data/2.5/forecast/daily/find?q="+ place +"&units=metric&appid=60dc55b1b64f048387de957a5efe4ab2";
+fetch(forecastapi)
+.then(response=>{
+    forecastdata=response.json();
+    return forecastdata;
+})
+.then(forecastdata=>{
+    console.log(forecastdata);})
+
+}
+let weatherIcon=document.querySelector('.weather-icon');
+
+//add today's date
+let date=document.querySelector('.date');
+let today=new Date();
+const options={year: 'numeric', month: 'long', day: 'numeric' }
+date.textContent=today.toLocaleDateString('en-US',options);
+
